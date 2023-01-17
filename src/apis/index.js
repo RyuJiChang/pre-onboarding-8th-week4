ec5d56ee1a48c918296059708d4c39b2b3193ca9
RyuJiChang <rgc0582@gmail.com>
2023-01-18 04:12:56 +0900

    feat: pagenation 구현

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const getPage = (page, limit) => {
-  axios
-    .get(
-      `http://localhost:4000/comments?_page=${1}&_limit=${4}&_order=desc&_sort=id`
-    )
-    .then((response) => {
-      console.log(response.data);
-    });
+const getPage = async (page, limit) => {
+  const { data } = await axios.get(
+    `http://localhost:4000/comments?_page=${page}&_limit=${limit}&_order=desc&_sort=id`
+  );
+  return data;
 };
 
 export { getPage };
--- src/components/PageList.js
+++ src/components/PageList.js
@@ -1,5 +1,117 @@
 import React from "react";
 import styled from "styled-components";
+import { getPage } from "../apis";
+
+function PageList({ aboutLists, setAboutLists }) {
+  let pages = [];
+  let maxPage = Math.ceil(Number(aboutLists.amount) / aboutLists.limit);
+  const pageHandler = (e) => {
+    if (e.target.textContent === "Prev" && e.target.textContent !== "1") {
+      getPage(aboutLists.page - 1, aboutLists.limit).then((newPage) => {
+        setAboutLists({
+          ...aboutLists,
+          lists: newPage,
+          page: aboutLists.page - 1,
+        });
+      });
+    } else if (
+      e.target.textContent === "Next" &&
+      e.target.textContent !== maxPage
+    ) {
+      getPage(aboutLists.page + 1, aboutLists.limit).then((newPage) => {
+        setAboutLists({
+          ...aboutLists,
+          lists: newPage,
+          page: aboutLists.page + 1,
+        });
+      });
+    } else if (Number(e.target.textContent)) {
+      getPage(Number(e.target.textContent), aboutLists.limit).then(
+        (newPage) => {
+          setAboutLists({
+            ...aboutLists,
+            lists: newPage,
+            page: Number(e.target.textContent),
+          });
+        }
+      );
+    }
+  };
+
+  //now = aboutLists.page
+  //dummyPagenation.pageInfo.totalPages = maxPage
+
+  if (Number(aboutLists.page) === 1) {
+    pages = [];
+    for (let i = 1; i <= Math.min(5, maxPage); i++) {
+      pages.push(i);
+    }
+    if (maxPage > 5) {
+      pages.push("...");
+      pages.push(maxPage);
+      pages.push("Next");
+    } else if (maxPage > 1) {
+      pages.push("Next");
+    }
+  } else if (Number(aboutLists.page) <= 4) {
+    pages = ["Prev"];
+    for (let i = 1; i <= Math.min(5, maxPage); i++) {
+      pages.push(i);
+    }
+    if (maxPage > 5) {
+      pages.push("...");
+      pages.push(maxPage);
+      pages.push("Next");
+    } else if (maxPage > Number(aboutLists.page)) {
+      pages.push("Next");
+    }
+  } else if (Number(aboutLists.page) >= 5) {
+    pages = ["Prev", 1, "..."];
+    for (
+      let i = Number(aboutLists.page) - 2;
+      i <= Math.min(Number(aboutLists.page) + 2, maxPage);
+      i++
+    ) {
+      pages.push(i);
+    }
+    if (maxPage === Number(aboutLists.page)) {
+    } else if (maxPage === Number(aboutLists.page) + 3) {
+      let anotherPage = pages.filter(
+        (el) => el !== Number(aboutLists.page) + -2
+      );
+      pages = [...anotherPage];
+      pages.push(Number(aboutLists.page) + 3);
+      pages.push("Next");
+    } else if (maxPage > Number(aboutLists.page) + 3) {
+      pages.push("...");
+      pages.push(maxPage);
+      pages.push("Next");
+    } else if (maxPage > Number(aboutLists.page)) {
+      pages.push("Next");
+    }
+  }
+
+  // pageArray.push(<Page key="1">1</Page>);
+
+  return (
+    <PageListStyle>
+      {pages.map((el) => (
+        <Page
+          onClick={pageHandler}
+          id={el}
+          key={el}
+          active={el === aboutLists.page ? true : false}
+          className={`${el === "..." ? "none" : "pageBox"}
+          }`}
+        >
+          {el}
+        </Page>
+      ))}
+    </PageListStyle>
+  );
+}
+
+export default PageList;
 
 const PageListStyle = styled.div`
   margin-bottom: 20px;
@@ -15,19 +127,8 @@ const Page = styled.button`
   ${({ active }) =>
     active &&
     `
-        background: gray;
-        color: #fff;
-  `}
+                  background: gray;
+                  color: #fff;
+            `}
   margin-right: 3px;
 `;
-
-function PageList({ aboutLists, setAboutLists }) {
-  const pageArray = [];
-  console.log(aboutLists);
-
-  pageArray.push(<Page key="1">1</Page>);
-
-  return <PageListStyle>{pageArray}</PageListStyle>;
-}
-
-export default PageList;
