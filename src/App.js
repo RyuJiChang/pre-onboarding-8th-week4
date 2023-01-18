7a661df52065b18c2af0aa2344bb9e41d7f14858
RyuJiChang <rgc0582@gmail.com>
2023-01-18 19:44:25 +0900

    chore: redux 설치

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
         `http://localhost:4000/comments?_page=${1}&_limit=${4}&_order=desc&_sort=id`
       )
       .then((response) => {
+        console.log(response);
         setAboutLists({
           ...aboutLists,
           page: 1,
